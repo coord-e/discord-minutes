f844db1cd1d6f6a6042deffecbac1d52c5e72a98
coord.e <me@coord-e.com>
2018-06-09 10:37:00 +0900

    Try audio recording

--- index.js
+++ index.js
@@ -5,14 +5,63 @@ const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+// make a new stream for each time someone starts to talk
+function generateOutputFile(channel, member) {
+  // use IDs instead of username cause some people have stupid emojis in their name
+  const fileName = `./recordings/${channel.id}-${member.id}-${Date.now()}.pcm`;
+  return fs.createWriteStream(fileName);
+}
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  if (msg.content.startsWith(config.prefix+'join')) {
+    let [command, ...channelName] = msg.content.split(" ");
+    if (!msg.guild) {
+      return msg.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
+    }
+    const voiceChannel = msg.guild.channels.find("name", channelName.join(" "));
+    //console.log(voiceChannel.id);
+    if (!voiceChannel || voiceChannel.type !== 'voice') {
+      return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
+    }
+    voiceChannel.join()
+      .then(conn => {
+        msg.reply('ready!');
+        // create our voice receiver
+        const receiver = conn.createReceiver();
+
+        conn.on('speaking', (user, speaking) => {
+          if (speaking) {
+            msg.channel.send(`I'm listening to ${user}`);
+            // this creates a 16-bit signed PCM, stereo 48KHz PCM stream.
+            const audioStream = receiver.createPCMStream(user);
+            // create an output stream so we can dump our data in a file
+            const outputStream = generateOutputFile(voiceChannel, user);
+            // pipe our audio data into the file stream
+            audioStream.pipe(outputStream);
+            outputStream.on("data", console.log);
+            // when the stream ends (the user stopped talking) tell the user
+            audioStream.on('end', () => {
+              msg.channel.send(`I'm no longer listening to ${user}`);
+            });
+          }
+        });
+      })
+      .catch(console.log);
   }
+  if(msg.content.startsWith(config.prefix+'leave')) {
+    let [command, ...channelName] = msg.content.split(" ");
+    let voiceChannel = msg.guild.channels.find("name", channelName.join(" "));
+    if (!voiceChannel || voiceChannel.type !== 'voice') {
+      return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
+    }
+    voiceChannel.leave();
+    return msg.reply("Left. Thanks");
+  }
+});
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(config.token);
